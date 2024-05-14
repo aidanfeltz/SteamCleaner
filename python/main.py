6266f029c235005de1f59fb07626bbb38faf6cbf
aidanfeltz <154996275+aidanfeltz@users.noreply.github.com>
2024-05-14 05:02:12 -0700

    Create main.py

---
+++ python/main.py
@@ -0,0 +1,29 @@
+import os
+import json
+import requests
+import tkinter as tk
+from tkinter import ttk
+import platform
+
+os = platform.system()
+#fining the 
+
+
+
+def simple_clean()
+    if os == "Windows"
+        path =
+    if os == "Linux"
+        path =
+    if os == "Darwin"
+        path =
+root = tk.Tk()
+
+
+
+
+
+ttk.label(root, text = 'Steam Cleaner').pack()
+button = tk.Button(root, text +"Clean", command = clean)
+root.mainloop()
+
